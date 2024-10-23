f2c5e013b98cff12c950d43b13c9c432ce440dc1
Roy <royyanwijaya@gmail.com>
2024-10-23 17:01:47 +0700

    change the profile modal layout and more
    Reduce the profile modal dialog width
    
    Wrap and separate the contents with card
    
    Add briefcase icon on the Experience title and wrap it with badge
    
    Add graduation cap icon on the Education title and wrap it with badge
    
    Make the page mobile-responsive
    
    Add no-wrap class to the table to make the text show fully

--- app/components/Leads.jsx
+++ app/components/Leads.jsx
@@ -117,13 +117,13 @@ export const columns = [
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuItem>
-              {/* <Button onClick={handleGenerateEmail}>
+              {/* <Button variant="outline" onClick={handleGenerateEmail}>
                 Generate personalized email
               </Button> */}
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleViewDetails}>
-              <Button>View profile details</Button>
+              <Button variant="outline">View profile details</Button>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
@@ -233,7 +233,7 @@ function Leads() {
 
   return (
     <div className="w-full max-w-5xl">
-      <div className="flex justify-between items-center py-4">
+      <div className="flex flex-col justify-between items-start py-4 space-y-4 pt-md-8 sm:flex-row sm:items-center sm:space-y-0">
         <div className="flex gap-2 items-start">
           <div className="flex items-center">
             <UserCheck className="w-6 h-6 text-blue-600" />
@@ -247,6 +247,7 @@ function Leads() {
               <a
                 href="https://nubela.co/proxycurl"
                 target="_blank"
+                rel="noopener noreferrer"
                 className="underline ms-1"
               >
                 Proxycurl
@@ -254,85 +255,87 @@ function Leads() {
             </span>
           </div>
         </div>
-        <div className="flex gap-4">
-          <Button variant="outline" onClick={handleAddLeads}>
-            <Plus className="w-4 h-4" /> Add Leads
+        <div className="flex flex-col gap-2 w-full sm:flex-row sm:gap-4 sm:w-auto">
+          <Button variant="outline" onClick={handleAddLeads} className="w-full sm:w-auto">
+            <Plus className="mr-2 w-4 h-4" /> Add Leads
           </Button>
-          <Button onClick={handleBulkGenerateEmail}>
-            <Mail className="w-4 h-4" /> Generate all personalized email
+          <Button onClick={handleBulkGenerateEmail} className="w-full sm:w-auto">
+            <Mail className="mr-2 w-4 h-4" /> Generate all personalized email
           </Button>
         </div>
       </div>
       <div className="rounded-md border">
-        <Table className="w-full">
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+        <div className="overflow-x-auto">
+          <Table className="w-full whitespace-nowrap">
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id} className="px-2">
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  <div className="flex flex-col justify-center items-center py-16 h-full">
-                    <Image
-                      src="/Illustration_landscape_placement.png"
-                      alt="empty illustration"
-                      width={400}
-                      height={400}
-                      className="mb-4 rounded-md"
-                    />
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id} className="px-2">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    <div className="flex flex-col justify-center items-center py-16 h-full">
+                      <Image
+                        src="/Illustration_landscape_placement.png"
+                        alt="empty illustration"
+                        width={400}
+                        height={400}
+                        className="mb-4 rounded-md"
+                      />
 
-                    <p className="text-gray-500">
-                      No leads found. Add LinkedIn profile URLs to start
-                      personalizing emails.
-                    </p>
+                      <p className="text-gray-500">
+                        No leads found. Add LinkedIn profile URLs to start
+                        personalizing emails.
+                      </p>
 
-                    <Button
-                      variant="outline"
-                      className="mt-4"
-                      onClick={handleAddLeads}
-                    >
-                      <Plus className="w-4 h-4" /> Add Leads
-                    </Button>
-                  </div>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+                      <Button
+                        variant="outline"
+                        className="mt-4"
+                        onClick={handleAddLeads}
+                      >
+                        <Plus className="w-4 h-4" /> Add Leads
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
       </div>
       <div className="flex justify-end items-center py-4 space-x-2">
         <div className="flex-1 text-sm text-muted-foreground">
